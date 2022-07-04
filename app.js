df9f6fad437e512f60d1d183c31aa17a574cb6c7
Tejas Hirpara <tejashirapara369@gmail.com>
2022-07-04 11:31:50 +0530

    nodemon added

--- app.js
+++ app.js
@@ -41,6 +41,8 @@ mongoose
   })
   .catch((e) => console.log('error', e))
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
   console.log('Server is running on port 3000')
 })
